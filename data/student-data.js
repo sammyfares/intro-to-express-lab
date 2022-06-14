570b4a2512f40467b4509f51b79497dd17a4295e
User Name <user@email.com>
2022-06-14 11:29:54 -0400

    completestudents list

--- data/student-data.js
+++ data/student-data.js
@@ -2,6 +2,7 @@ const students = [
     {text: 'Feed llama', done: true, _id: 125223},
     {text: 'Sleep under the stars', done: false, _id: 127904},
     {text: 'Buy milk', done: false, _id: 139608},
+    {text: 'Mike', done: false, _id: 139608},
   ]
   
   export { 
